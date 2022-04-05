27ca758205f367c663cfd86f0adb362febc03e15
agustinVon <agustin.von@ing.austral.edu.ar>
2022-04-05 15:23:51 -0300

    fix: changed ast nodes

--- commons/src/main/scala/ast/ASTree.scala
+++ commons/src/main/scala/ast/ASTree.scala
@@ -1,9 +1,19 @@
 package ast
 
+import org.austral.ingsis.printscript.common.TokenType
 import org.austral.ingsis.printscript.parser.Content
 
 
 sealed trait ASTree
-case class Leaf(value: Content[String]) extends ASTree
-case class Branch(branches: List[ASTree], node: Content[String]) extends ASTree
-case class StringBranch(branches: List[ASTree], node: String) extends ASTree
+case class Root(sentences: List[ASTree]) extends ASTree
+case class Declaration(declaration: Content[String], id: Content[String], declType: Content[String]) extends ASTree
+sealed trait Expression extends ASTree
+case class LiteralString(value: Content[String]) extends Expression
+case class LiteralNumber(value: Content[Int]) extends Expression
+case class Variable(value: Content[String]) extends Expression
+case class Operation(exp1: Expression, operator: Content[String], exp2: Expression) extends Expression
+case class Operator(operator: Content[String]) extends ASTree
+case class PrintLn(function: Content[String], expression: Expression) extends ASTree
+case class DeclarationAssignation(declaration: Declaration, assignation: Content[String], expression: Expression)
+case class VariableAssignation(variable: Variable, assignation:Content[String], expression: Expression)
+
