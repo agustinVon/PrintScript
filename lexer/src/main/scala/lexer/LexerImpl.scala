8b1a57166e9084d61890d0edaf2de4fca0297b01
germanbuchmuller <german.buchmuller@ing.austral.edu.ar>
2022-04-05 12:25:33 -0300

    Modularized Lexer

--- lexer/src/main/scala/lexer/LexerImpl.scala
+++ lexer/src/main/scala/lexer/LexerImpl.scala
@@ -3,42 +3,59 @@ package lexer
 import tokens.TokenTypesImpl
 import org.austral.ingsis.printscript.common.{LexicalRange, Token, TokenType}
 
-class LexerImpl extends Lexer{
+import java.util.regex.{MatchResult, Matcher}
+
+class LexerImpl extends Lexer {
+
+  type MatchResult = (String, Option[TokenType])
+  type TokenGenerationResult = (Option[Token], Int, Int, Int)
   val matchers: List[(TokenType, LexerMatcher)] = Matches().getMatches
+
   override def lex(source: String): List[Token] = {
-    val matcher = LexerMatcherImpl.fromMatchers(matchers.map(_._2)).getMatcher(source)
+    val matcher = getMainMatcher(source)
+    getTokensListFromMatcher(matcher)
+  }
+
+  private def getMainMatcher(source:String):Matcher = {
+    LexerMatcherImpl.fromMatchers(matchers.map(_._2)).getMatcher(source)
+  }
+
+  private def getTokensListFromMatcher(matcher: Matcher):List[Token] = {
     var tokens: List[Token] = List()
-    var line = 0
-    var position = 0
-    var column = 0
+    var line, position, column = 0
     while (matcher.find()) {
-      val lexMatch = matcher.group()
-      val possibleTType = matchers.map(_._1).find(key => matcher.group(key.getType) != null)
-      possibleTType match {
-        case None => {
-          throw new RuntimeException
-        }
-        case Some(TokenTypesImpl.WHITESPACE) => {
-          column = lexMatch.length + column
-          position = lexMatch.length + position
-        }
-        case Some(TokenTypesImpl.EOL) => {
-          column = 0
-          position = lexMatch.length + position
-          line = line + 1
-        }
-        case Some(x) => {
-          tokens = tokens :+ new Token(x, position, position + lexMatch.length, new LexicalRange(column, line, column + lexMatch.length, line ))
-          column = lexMatch.length + column
-          position = lexMatch.length + position
-        }
+      val matchResult:MatchResult = getMatchResult(matcher)
+      val (token, l, p, c) = createTokenFromMatchResult(matchResult:MatchResult, line:Int, position:Int, column:Int)
+      token match {
+        case Some(x) => tokens = tokens:+ x
+        case None =>
       }
+      line = l
+      position = p
+      column = c
+    }
+    tokens :+ new Token(TokenTypesImpl.EOF, position, position, new LexicalRange(column, line, column, line))
+  }
+
+  private def getMatchResult(matcher:Matcher):MatchResult = {
+    (matcher.group(), matchers.map(_._1).find(key => matcher.group(key.getType) != null))
+  }
+
+  private def createTokenFromMatchResult(matchResult: MatchResult, line: Int, position: Int, column: Int): TokenGenerationResult = {
+    matchResult._2 match {
+      case None => {
+        (None, line, position, column)
       }
-    tokens :+ new Token(
-      TokenTypesImpl.EOF,
-      position,
-      position,
-      new LexicalRange(column, line, column, line)
-    )
+      case Some(TokenTypesImpl.WHITESPACE) => {
+        (None, line, matchResult._1.length + position, matchResult._1.length + column)
+      }
+      case Some(TokenTypesImpl.EOL) => {
+        (None, line + 1, matchResult._1.length + position, 0)
+      }
+      case Some(x) => {
+        (Some(new Token(x, position, position + matchResult._1.length, new LexicalRange(column, line, column + matchResult._1.length, line ))), line, matchResult._1.length + position, matchResult._1.length + column)
+      }
+    }
   }
 }
+
