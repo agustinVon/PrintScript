0baf3e0e25c852df448117eb32577a8871c44df7
germanbuchmuller <german.buchmuller@ing.austral.edu.ar>
2022-05-02 22:35:01 -0300

    test: git hook pre commit

--- interpreter/src/main/scala/interpreter/InterpreterImpl.scala
+++ interpreter/src/main/scala/interpreter/InterpreterImpl.scala
@@ -40,7 +40,7 @@ case class InterpreterImpl() extends Interpreter {
   private var constantValues: Map[String, Option[Any]]         = Map()
   private var validationPhase: Boolean                         = false
   private val operationSolver: OperationSolver                 = OperationSolverImpl()
-  private var displayMethod: DisplayMethod  = PrintScriptPrinter()
+  private var displayMethod: DisplayMethod                     = PrintScriptPrinter()
   private var inputMethod: InputMethod                         = PrintScriptInput()
 
   override def interpret(ast: ASTree, displayMethod: DisplayMethod, input: InputMethod): Unit = {
